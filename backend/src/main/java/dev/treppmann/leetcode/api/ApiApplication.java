2517826264298fbd35e1304b371457f773de175c
Patrick <patrick.treppmann97@gmail.com>
2023-12-07 16:11:31 +0100

    added another example entry to db

--- backend/src/main/java/dev/treppmann/leetcode/api/ApiApplication.java
+++ backend/src/main/java/dev/treppmann/leetcode/api/ApiApplication.java
@@ -42,7 +42,7 @@ public class ApiApplication {
 
 			Problem binarySearch = new Problem();
 			binarySearch.setDescription(List.of("Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1."));
-			binarySearch.setDifficulty(Difficulty.EASY);
+			binarySearch.setDifficulty(Difficulty.MEDIUM);
 			binarySearch.setOptimalComplexity("O(log n) time | O(1) space - where n is the length of the input array");
 			binarySearch.setCategories(List.of(Category.ARRAY));
 			binarySearch.setProblemName("Binary Search");
@@ -55,6 +55,22 @@ public class ApiApplication {
 			placeHolderCode.put(ProgrammingLanguage.JAVASCRIPT, "const binarySearch = (array, target) => {\n\t// Write your code here\n}");
 			binarySearch.setPlaceHolderCode(placeHolderCode);
 			repository.save(binarySearch);
+
+			Problem binarySearch2 = new Problem();
+			binarySearch2.setDescription(List.of("Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1."));
+			binarySearch2.setDifficulty(Difficulty.HARD);
+			binarySearch2.setOptimalComplexity("O(log n) time | O(1) space - where n is the length of the input array");
+			binarySearch2.setCategories(List.of(Category.ARRAY));
+			binarySearch2.setProblemName("Binary Search");
+			binarySearch2.setProblemId("binary-search-2");
+			binarySearch2.setHints(List.of("The Binary Search algorithm works by finding the number in the middle of the input array and comparing it to the target number. Given that the array is sorted, if this middle number is smaller than the target number, then the entire left part of the array is no longer worth exploring since the target number can no longer be in it; similarly, if the middle number is greater than the target number, then the entire right part of the array is no longer worth exploring. Applying this logic recursively eliminates half of the array until the number is found or until the array runs out of numbers.", "Write a helper function that takes in two additional arguments: a left pointer and a right pointer representing the indices at the extremities of the array (or subarray) that you are applying Binary Search on. The first time this helper function is called, the left pointer should be zero and the right pointer should be the final index of the input array. To find the index of the middle number mentioned in Hint #1, simply round down the number obtained from: (left + right) / 2. Apply this logic recursively until you find the target number or until the left pointer becomes greater than the right pointer.", "Can you implement this algorithm iteratively? Are there any advantages to doing so?"));
+			binarySearch2.setSampleInput("array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]");
+			binarySearch2.setSampleOutput("3");
+			placeHolderCode = new HashMap<>();
+			placeHolderCode.put(ProgrammingLanguage.PYTHON, "def binary_search(array, target):\n\t# Write your code here\n\tpass");
+			placeHolderCode.put(ProgrammingLanguage.JAVASCRIPT, "const binarySearch = (array, target) => {\n\t// Write your code here\n}");
+			binarySearch2.setPlaceHolderCode(placeHolderCode);
+			repository.save(binarySearch2);
 		};
 	}
 
